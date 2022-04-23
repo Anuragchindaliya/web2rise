aae78970cd5ac3ac9a4c7453e8468e469afa6e0c
Anuragchindaliya <ak1148383@gmail.com>
2022-04-23 17:22:04 +0530

    add:header, home,about

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,38 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    defaultTitle: "web2rise",
+    defaultDescription:
+      "WELCOME TO WEB2RISE Your Success Is Our Purpose Learn more contact us Quality Services Website Designing We endures our client demands at scale and offers high-quality customized business solutions. View More Digital Marketing We do Digital Marketing, SEO/SEM, database marketing, email, social media, and display advertising campaigns. View More App Development With our talented team ... Read more",
+    siteUrl: `https://www.web2rise.com`,
   },
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-transformer-json",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    `gatsby-transformer-sharp`,
+
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `services`,
+        path: `${__dirname}/services`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `bakcgroundImage`,
+        path: `${__dirname}/assets/bg`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `assets`,
+        path: `${__dirname}/assets`,
+      },
+    },
+    "gatsby-plugin-mdx",
+  ],
+};
